51d0687822b995d9c25a4be93f0f518753fa3c9c
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-09-16 20:59:00 +0530

    barcode scan disabled

--- src/components/ScannerModel.jsx
+++ src/components/ScannerModel.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { BarcodeScanner } from "@thewirv/react-barcode-scanner";
+// import React, { useEffect, useState } from "react";
+// import { BarcodeScanner } from "@thewirv/react-barcode-scanner";
 
-const ScannerModel = ({ barcodeScannerValue }) => {
-  return (
-    <>
-      <BarcodeScanner
-        onSuccess={barcodeScannerValue}
-        onError={(error) => {
-          if (error) {
-            console.error(error.message);
-          }
-        }}
-        onLoad={() => console.log("Video feed has loaded!")}
-        // containerStyle={{ width: "100%" }}
-        containerStyle={{
-          width: "100%",
-          height: "100%",
-          maxWidth: "640px",
-          maxHeight: "480px",
-        }}
-      />
-    </>
-  );
-};
-//sajidd
+// const ScannerModel = ({ barcodeScannerValue }) => {
+//   return (
+//     <>
+//       <BarcodeScanner
+//         onSuccess={barcodeScannerValue}
+//         onError={(error) => {
+//           if (error) {
+//             console.error(error.message);
+//           }
+//         }}
+//         onLoad={() => console.log("Video feed has loaded!")}
+//         // containerStyle={{ width: "100%" }}
+//         containerStyle={{
+//           width: "100%",
+//           height: "100%",
+//           maxWidth: "640px",
+//           maxHeight: "480px",
+//         }}
+//       />
+//     </>
+//   );
+// };
+// //sajidd
 
-export default ScannerModel;
+// export default ScannerModel;
